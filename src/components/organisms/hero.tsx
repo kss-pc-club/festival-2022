1cad727effb786993d24d31f8a9efa753212abe0
Ryoga-exe <ryoga.dev@gmail.com>
2022-06-03 15:03:03 +0900

    update: workflow

--- src/components/organisms/hero.tsx
+++ src/components/organisms/hero.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import * as THREE from 'three'
 
