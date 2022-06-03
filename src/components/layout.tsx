f75f67dd82952d56e5447da86aa8c155f9684e36
Ryoga-exe <ryoga.dev@gmail.com>
2022-06-03 15:17:00 +0900

    update

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,10 +4,9 @@ import Footer from 'src/components/organisms/footer'
 import Header from 'src/components/organisms/header'
 
 const Layout: React.FC<any> = (Props) => {
-  const isRootPath = Props.location.pathname === `${__PATH_PREFIX__}/`
   return (
     <React.Fragment>
-      {isRootPath ? <header></header> : <Header />}
+      <Header />
       <main className={Props.styles}>{Props.children}</main>
       <Footer />
     </React.Fragment>
